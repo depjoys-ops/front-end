f11131e675397d6b3b1b8aade97675e40c119e96
Oleh Kuzmych <depjoys@gmail.com>
2025-08-05 12:06:13 +0000

    change port

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -12,8 +12,8 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 5000")
-	err := http.ListenAndServe(":5000", nil)
+	fmt.Println("Starting front end service on port 4002")
+	err := http.ListenAndServe(":4002", nil)
 	if err != nil {
 		log.Panic(err)
 	}
